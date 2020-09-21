03a3d516f939bcacbba3acf432d7c8e4facba5b9
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-09-21 04:12:13 +0000

    simple bigram no smoothing

--- ngram.py
+++ ngram.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # (c) 2020 Sylvain Le Groux <slegroux@ccrma.stanford.edu>
 
-import pandas as pd
-from IPython import embed
-from typing import List, Set
+from typing import List, Set, Dict
 import math
 import numpy as np
 import logging
+from collections import Counter
+# from IPython import embed
 
 logger = logging.getLogger('language_modeling')
 logger.setLevel(logging.DEBUG)
@@ -26,14 +26,15 @@ logger.addHandler(ch)
 
 
 class Corpus(object):
-    def __init__(self, path:str, add_bos=True, add_eos=True):
+    def __init__(self, path:str, add_bos:bool=True, add_eos:bool=True):
+        logger.info('Parse and format corpus')
         self._corpus = []
         self._vocab = set()
         self._path = path
         self._num_sentences = 0
         self.parse_and_format_corpus(self._path, add_bos, add_eos)
 
-    def parse_and_format_corpus(self, path:str, add_bos=True, add_eos=True):
+    def parse_and_format_corpus(self, path:str, add_bos:bool=True, add_eos:bool=True):
         try:
             with open(path, 'r', encoding='utf-8') as f:
                 for line in f:
@@ -69,89 +70,70 @@ class Corpus(object):
         return(self._num_sentences)
 
 
-class LanguageModel(object):
-    def __init__(self, corpus:Corpus, order=1):
-        self._corpus = corpus
-        self._unigrams = {}
-        self._unigram_counter = {}
-        self._bigrams = {}
-        self._bigram_counter = {}
-        self._trigrams = {}
-        if order == 1:
-            self.compute_unigrams()
-        elif order == 2:
-            self.compute_unigrams()
-            self.compute_bigrams()
-    
-    def compute_unigrams(self):
-        words_count = 0
-        for sentence in self._corpus:
-            for word in sentence:
-                words_count += 1
-                if word in self._unigrams.keys():
-                    self._unigrams[word] += 1
-                else:
-                    self._unigrams[word] = 1
-        for k,v in self._unigrams.items():
-            self._unigrams[k] = math.log10(v/words_count)
-    
-    def compute_bigrams(self):
-        if not self._unigrams:
-            self.compute_unigrams()
-        bigrams_count = 0 # tot nber of bigrams
-        for sentence in self._corpus:
-            for i in range(len(sentence)-1):
-                bg = (sentence[i], sentence[i+1])
-                print(bg)
-                bigrams_count += 1
-                if bg in self._bigram_counter.keys():
-                    self._bigram_counter[bg] += 1
-                else:
-                    self._bigram_counter[bg] = 1
-
-        for k,v in self._bigram_counter.items():
-            # P(b|a) = P(a&b)/P(a)
-            self._bigrams[k] = math.log10(self._bigram_counter[k] / bigrams_count) - self._unigrams[k[0]]
-    
-    @property
-    def vocabulary(self):
-        return(set(self._unigrams.keys()))
-
-
 class Unigram(object):
     def __init__(self, corpus:List[List[str]]):
         logger.info("read corpus")
         self._corpus = corpus
-        logger.info("count unigrams")
-        self._counts = {}
+        self._unigram_counter = {}
         self._probs = {}
         self._tot_count = 0
+        logger.info("count unigrams")
         self.count_unigrams()
     
     def count_unigrams(self):
         for sentence in self._corpus:
             for word in sentence:
                 self._tot_count += 1
-                if word in self._counts:
-                    self._counts[word] += 1
+                if word in self._unigram_counter:
+                    self._unigram_counter[word] += 1
                 else:
-                    self._counts[word] = 1
+                    self._unigram_counter[word] = 1
     
-        for word in self._counts:
-            self._probs[word] = self._counts[word] / self._tot_count
+        for word in self._unigram_counter:
+            self._probs[word] = self._unigram_counter[word] / self._tot_count
 
     def generate(self)->float:
         return(np.random.choice(list(self._probs.keys()),p=list(self._probs.values())))
 
     @property
-    def counts(self):
-        return(self._counts)
+    def counts(self)->Dict:
+        return(self._unigram_counter)
     
     @property
-    def probs(self):
+    def probs(self)->Dict:
         return(self._probs)
     
     @property
-    def tot_count(self):
+    def tot_count(self)->int:
         return(self._tot_count)
 
+
+class Bigram(Unigram):
+    def __init__(self, corpus:List[List[str]]):
+        Unigram.__init__(self, corpus)
+        self._corpus = corpus
+        self._bigram_counter = Counter()
+        self.count_bigrams()
+        self._probs = {}
+        self.get_cond_probs()
+
+    def count_bigrams(self):
+        for line in self._corpus:
+            self._bigram_counter.update([ (line[i], line[i+1]) for i in range(len(line)-1)])
+    
+    def get_cond_probs(self):
+        # P(B|A) = C(A,B) / C(A)
+        for k in self._bigram_counter:
+            # count bigrams starting with k[0] in counter == count unigrams k[0]
+            self._probs[k] = self._bigram_counter[k] / self._unigram_counter[k[0]]
+
+    def generate(self):
+        condition = '<s>'
+
+    @property
+    def counts(self)->Dict:
+        return(dict(self._bigram_counter))
+    
+    @property
+    def probs(self)->Dict:
+        return(self._probs)
--- test_ngram.py
+++ test_ngram.py
@@ -3,7 +3,7 @@
 # pytest -p no:warnings -svDATA_FOLDER +'.py
 
 import pytest
-from lm import Corpus, Unigram
+from lm import Corpus, Unigram, Bigram
 from IPython import embed
 import os
 
@@ -53,12 +53,29 @@ def test_unigram_generate(data):
         seq.append(u.generate())
     print(seq)
 
+def test_bigram_count(data):
+    c = Corpus(data['corpus'])
+    b = Bigram(c.corpus)
+    
+    assert b.counts == {('<s>', 'i'): 2, ('i', 'am'): 2, ('am', 'sam'): 1, ('sam', '</s>'): 1, \
+        ('<s>', 'sam'): 1, ('sam', 'i'): 1, ('am', '</s>'): 1, ('i', 'do'): 1, ('do', 'not'): 1, \
+        ('not', 'like'): 1, ('like', 'green'): 1, ('green', 'eggs'): 1, ('eggs', 'and'): 1, \
+        ('and', 'ham'): 1, ('ham', '</s>'): 1}
+    
+    assert b.probs == {('<s>', 'i'): 0.6666666666666666, ('i', 'am'): 0.6666666666666666, \
+        ('am', 'sam'): 0.5, ('sam', '</s>'): 0.5, ('<s>', 'sam'): 0.3333333333333333, \
+        ('sam', 'i'): 0.5, ('am', '</s>'): 0.5, ('i', 'do'): 0.3333333333333333, \
+        ('do', 'not'): 1.0, ('not', 'like'): 1.0, ('like', 'green'): 1.0, \
+        ('green', 'eggs'): 1.0, ('eggs', 'and'): 1.0, ('and', 'ham'): 1.0, ('ham', '</s>'): 1.0}
+
 def test_large_corpus(data):
     # brown corpus already has eos/beos added to text file
     c = Corpus(data['large_corpus'], add_eos=False, add_bos=False)
     u = Unigram(c.corpus)
+    b = Bigram(c.corpus)
     assert u.probs['all'] == 0.0004051863857374392
     assert u.probs['resolution'] == 0.003646677471636953
-
-def test_unigram(data):
-    pass
+    # p(the|all)
+    assert b.probs[('all', 'the')] == 1.0
+    # p(jury|the)
+    assert b.probs[('the', 'jury')] == 0.08333333333333333
