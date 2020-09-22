98cc1fab68b2b98fe92c6fe7e1d198acdeb811d8
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-09-22 02:01:40 +0000

    update info and version

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 PROJECT_NAME='slgasr'
-VERSION='0.0.0'
+VERSION='0.0.1'
 AUTHOR='Sylvain Le Groux'
-EMAIL='slegroux@ccrma.stanford.edu',
+EMAIL='slegroux@ccrma.stanford.edu'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name=PROJECT_NAME + '-slegroux',
     version=VERSION,
     author=AUTHOR,
@@ -12,5 +16,14 @@ setup(name=PROJECT_NAME + '-slegroux',
     scripts=['slgasr/bin/run_tests.sh', 'slgasr/bin/format_commonvoice.py'],
     package_data={'slgasr': ['slgasr/data/*']},
     license='LICENSE',
-    description="utils for speech recognition"
+    description="A set of utilities for speech recognition",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/slegroux/slgASR",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
 )
